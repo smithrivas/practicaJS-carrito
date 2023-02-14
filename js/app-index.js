8696187c1f3634b695c809d912722babb906ddd2
Brayan <smithrivas.dev@gmail.com>
2023-02-13 22:03:21 -0500

    Se agrega opciones de filtrado con JS en página principal de cursos

--- js/app-index.js
+++ js/app-index.js
@@ -1,35 +1,142 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    // Mostrar cursos
+    // Mostrar todos los cursos
     showCourses(cursos);
 });
 
+// CONTENEDOR PARA LOS RESULTADOS
+const result = document.querySelector('#principal-content');
+
 // SELECTORES
-const card = document.querySelector('#principal-content');
+const category = document.querySelector('#categoria');
+const min = document.querySelector('#minimo');
+const max = document.querySelector('#maximo');
+const reset = document.querySelector('#reset');
+
+// OBJETO QUE SE LLENARÁ CON LA BUSQUEDA
+const data = {    
+    categoria: '',
+    minimo: '',
+    maximo: ''
+}
+
+// EVENT LISTENERS PARA LOS FILTROS
+category.addEventListener('change', e=>{
+    data.categoria = e.target.value;
+    filterCourses();
+});
+min.addEventListener('change', e=>{
+    data.minimo = parseInt(e.target.value);
+    filterCourses();
+});
+max.addEventListener('change', e=>{
+    data.maximo = parseInt(e.target.value);
+    filterCourses();
+});
+
+// RESETEAR FILTROS
+reset.addEventListener('click',()=>{    
+    data.categoria = '';
+    data.minimo = '';
+    data.maximo = '';
+    category.value = '';
+    min.value = '';
+    max.value = '';
+    filterCourses(data);    
+});
 
 // FUNCIONES
 function showCourses(cursos){
+    clearHTML(); // Limpia el HTML
     cursos.forEach(curso => {
+        const {imagen, nombre, categoria, descripcion, precio} = curso;
+        // Crear los elementos
         const divCourseHTML = document.createElement('DIV');        
         const imgCourseHTML = document.createElement('IMG');
         const hCourseHTML = document.createElement('H2');
+        const catCourseHTML = document.createElement('P');
         const p1CourseHTML = document.createElement('P');
         const p2CourseHTML = document.createElement('P');
         const sCourseHTML = document.createElement('SPAN');
         const bCourseHTML = document.createElement('BUTTON');
 
+        // Crear los cards
         divCourseHTML.classList.add('card');
-        imgCourseHTML.src = 'img/'+curso.imagen;        
-        hCourseHTML.textContent = curso.nombre;
-        p1CourseHTML.textContent = curso.descripcion;
+        imgCourseHTML.src = 'img/'+imagen;        
+        hCourseHTML.textContent = nombre;
+        catCourseHTML.textContent = 'Categoria: '+categoria;
+        catCourseHTML.classList.add('category');
+        p1CourseHTML.textContent = descripcion;
         p2CourseHTML.textContent = 'Precio: ';
-        sCourseHTML.textContent = '$'+curso.precio;
+        sCourseHTML.textContent = '$'+precio;
         bCourseHTML.textContent = 'comprar';
         divCourseHTML.appendChild(imgCourseHTML);
         divCourseHTML.appendChild(hCourseHTML);
+        divCourseHTML.appendChild(catCourseHTML);
         divCourseHTML.appendChild(p1CourseHTML);
         p2CourseHTML.appendChild(sCourseHTML);
         divCourseHTML.appendChild(p2CourseHTML);
         divCourseHTML.appendChild(bCourseHTML);
-        card.appendChild(divCourseHTML)
+        
+        // Insertar en el HTML
+        result.appendChild(divCourseHTML)
     });
+}
+
+// LIMPIAR HTML
+function clearHTML(){
+    while(result.firstChild){
+        result.removeChild(result.firstChild);
+    }
+}
+
+// FILTRAR CURSOS
+function filterCourses(){    
+    const result = cursos.filter(filterCategory).filter(filterMin).filter(filterMax);    
+    if (result.length) {
+        return showCourses(result);
+    }else{
+        noResult();
+    }
+}
+
+// FILTRAR POR CATEGORIA
+function filterCategory(curso){
+    const {categoria} = data;
+    if (categoria) {
+        return curso.categoria === categoria;
+    }
+    return curso;
+}
+
+// FILTRAR POR PRECIO MÍNIMO
+function filterMin(curso){
+    const {minimo} = data;
+    if (minimo) {
+        return curso.precio >= minimo;
+    }
+    return curso;
+}
+
+// FILTRAR POR PRECIO MÁXIMO
+function filterMax(curso){
+    const {maximo} = data;
+    if (maximo) {
+        return curso.precio <= maximo;
+    }
+    return curso;
+}
+
+// SIN RESULTADO
+function noResult(){
+    clearHTML();
+    const noResult = document.createElement('DIV');
+    noResult.classList.add('container-no-result');
+    const pNoResult = document.createElement('P');
+    pNoResult.classList.add('no-result');
+    pNoResult.textContent = 'No hay resultados, intenta seleccionando otras opciones';
+    const error404 = document.createElement('IMG');
+    error404.src = 'img/404.jpg';
+    noResult.appendChild(pNoResult);
+    noResult.appendChild(error404);
+    result.appendChild(noResult);
 }
--- js/db.js
+++ js/db.js
@@ -2,85 +2,85 @@ const cursos = [
 	{
 		imagen: 'excel.png',
 		nombre: 'Curso Excel',
-		categoria: 'Ofimatica',
+		categoria: 'ofimatica',
         descripcion: 'Curso de microsoft excel que comprende desde el nivel básico al nivel experto.',
 		precio: 25
 	},
 	{
 		imagen: 'word.png',
 		nombre: 'Curso Word',
-		categoria: 'Ofimatica',
+		categoria: 'ofimatica',
         descripcion: 'Curso de microsoft word que comprende desde el nivel básico al nivel experto.',
-		precio: 25
+		precio: 30
 	},
     {
 		imagen: 'powerpoint.jpg',
 		nombre: 'Curso Powerpoint',
-		categoria: 'Ofimatica',
+		categoria: 'ofimatica',
         descripcion: 'Curso de microsoft powerpoint que comprende desde el nivel básico al nivel experto.',
 		precio: 25
 	},
     {
 		imagen: 'office.png',
 		nombre: 'Curso Microsoft Office',
-		categoria: 'Ofimatica',
+		categoria: 'ofimatica',
         descripcion: 'Curso de microsoft office que incluye toda su suite desde el nivel básico al nivel experto.',
 		precio: 50
 	},
     {
 		imagen: 'js.png',
 		nombre: 'Curso Javascript',
-		categoria: 'Programacion',
+		categoria: 'programacion',
         descripcion: 'Curso completo de Javascript',
-		precio: 40
+		precio: 35
 	},
     {
 		imagen: 'react.png',
 		nombre: 'Curso React básico',
-		categoria: 'Programacion',
+		categoria: 'programacion',
         descripcion: 'Curso de React para principiantes',
 		precio: 20
 	},
     {
 		imagen: 'java.png',
 		nombre: 'Curso Java',
-		categoria: 'Programacion',
+		categoria: 'programacion',
         descripcion: 'Curso completo de Java',
 		precio: 25
 	},
     {
 		imagen: 'htmlcss.jpg',
 		nombre: 'Curso HTML y CSS',
-		categoria: 'Programacion',
+		categoria: 'programacion',
         descripcion: 'Curso completo de HTML y CSS para principiantes',
 		precio: 40
 	},
     {
 		imagen: 'libros.jpg',
 		nombre: 'Curso de creación de libros digitales',
-		categoria: 'Tecnologia',
+		categoria: 'tecnologia',
         descripcion: 'Curso que te enseña como crear libros digitales desde cero',
-		precio: 16
+		precio: 15
 	},
     {
 		imagen: 'seguridad.jpg',
 		nombre: 'Curso basico de seguridad informatica',
-		categoria: 'Tecnologia',
+		categoria: 'tecnologia',
         descripcion: 'Curso de introducción a la seguridad informatica',
 		precio: 20
 	},
     {
 		imagen: 'mantenimiento.jpg',
 		nombre: 'Curso basico de mantenimiento de computadores',
-		categoria: 'Tecnologia',
+		categoria: 'tecnologia',
         descripcion: 'Curso de mantenimiento de computadores de cero a experto',
 		precio: 40
 	},
     {
 		imagen: 'videojuegos.jpg',
 		nombre: 'Curso de creación de videojuegos',
-		categoria: 'Tecnologia',
+		categoria: 'tecnologia',
         descripcion: 'Curso basico de desarrollo de videojuegos',
-		precio: 40
+		precio: 45
 	}
 ];
